71a5df4312db90d742af07712bc87d02314f87dc
Nitul Das <ndas6732@gmail.com>
2024-12-19 04:12:28 +0530

    Created a new tic tac toe game using react.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Game from "./Components/Game";
+
+const App = () => {
+    return (
+        <div className="App">
+            <Game />
+        </div>
+    );
+};
+
+export default App;
